dfb36a905773cd04e2444bc16e22943e12460407
JoaquimMateus0404 <joaquimmateus0404@gmail.com>
2025-07-22 19:02:32 +0100

    Adiciona script.js e atualiza id do checkbox de administrador no index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+let name = document.querySelector("#exampleInputName");
+let gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+let birth = document.querySelector("#exampleInputNameBirth");
+let country = document.querySelector("#exampleInputCountry");
+let email = document.querySelector("#exampleInputEmail");
+let password = document.querySelector("#exampleInputPassword");
+let photo = document.querySelector("#exampleInputFile");
+let admin = document.querySelector("#exampleInputAdmin");
