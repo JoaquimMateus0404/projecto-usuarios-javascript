350c31948d1003c192156463a661cb36c209973a
JoaquimMateus0404 <joaquimmateus0404@gmail.com>
2025-07-22 20:21:17 +0100

    Adiciona função para adicionar linhas na tabela de usuários e validação de campos no formulário

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
+function addLine(data) {
+    var tr = document.createElement("tr");
+    tr.innerHTML = `
+                <tr>
+                    <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                    <td>${data.name}</td>
+                    <td>${data.email}</td>
+                    <td>${data.admin}</td>
+                    <td>${data.birth}</td>
+                    <td>
+                      <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                      <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                    </td>
+                </tr>
+                `;
+    document.getElementById("table-users").appendChild(tr);
+}
+function validateFields() {
+    fields.forEach(function(field, index) {
+        if (field.value == "") {
+            field.classList.add("invalid");
+        } else {
+            field.classList.remove("invalid");
+        }
+    });
+}
 
 document.querySelector("#form-user-create").addEventListener("submit", function(event){
     event.preventDefault();
@@ -13,8 +39,8 @@ document.querySelector("#form-user-create").addEventListener("submit", function(
         }
     });
 
-    console.log(user);
-    
+    addLine(user);
+
     document.querySelector("#form-user-create").reset();
 
 });
