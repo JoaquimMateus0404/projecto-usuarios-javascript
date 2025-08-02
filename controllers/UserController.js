5dcb877f1ec5f7a89f9d7c517970f6e1234e836d
JoaquimMateus0404 <joaquimmateus0404@gmail.com>
2025-08-02 15:47:52 +0100

    Atualiza UserController para armazenar referência do formulário e ajusta a coleta de valores do usuário; remove código duplicado em script.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,20 @@
 class UserController {
     constructor(formEl){
-        var formEl = document.querySelector(formEl);
+        this.formEl = document.querySelector(formEl);
     }
 
     getValues(){
-        
+
+        this.formEl.Elements.forEach(function(field, index){
+            if (field.name == "gender") { 
+                if (field.checked)  user[field.name] = field.value;
+            }else {
+               user[field.name] = field.value;
+            }
+        });
+    
+        var objectUser = new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
+
+        return objectUser;
     }
 }
--- script.js
+++ script.js
@@ -29,15 +29,7 @@ function validateFields() {
 document.querySelector("#form-user-create").addEventListener("submit", function(event){
     event.preventDefault();
 
-    fields.forEach(function(field, index){
-        if (field.name == "gender") { 
-            if (field.checked)  user[field.name] = field.value;
-        }else {
-           user[field.name] = field.value;
-        }
-    });
-
-    var objectUser = new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin);
+    
 
     addLine(objectUser);
 
